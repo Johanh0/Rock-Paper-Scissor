2b394e5c3d215d12da5be845084e75eb67ee6f39
Johan Herrera <johanherrera20000@gmail.com>
2024-10-31 12:51:45 -0400

    Progress on the logic of the game

--- js/app.js
+++ js/app.js
@@ -8,36 +8,66 @@ const scissorBtn = document.querySelector("#game--scissor");
 const allGameBtns = document.querySelectorAll(".game__option");
 const playBtnGame = document.querySelector("#play__btn--game");
 
+const gameValues = {
+  crowd: "👑",
+  tie: "",
+  rock: "👊",
+  paper: "🖐️",
+  scissor: "✌️",
+};
+
+const changeIcon = () => {
+  let totalSelect = 0;
+
+  setTimeout(() => {
+    allGameBtns.forEach((gameBtn) => {
+      if (gameBtn.classList.contains("selected")) {
+        totalSelect = totalSelect + 1;
+        togglePlayBtn();
+        totalSelect > 1 ? clearSelected() : "";
+      }
+    });
+  }, 10);
+};
+
 const isSelected = () => {
   // Check if any of these elements has the class selected
   const rockBtnIsActive = rockBtn.classList.contains("selected");
   const paperBtnIsActive = paperBtn.classList.contains("selected");
   const scissorBtnIsActive = scissorBtn.classList.contains("selected");
-  let totalSelect = 0;
-
-  for (const gameBtn of allGameBtns) {
-    if (gameBtn.classList.contains("selected")) {
-      totalSelect = totalSelect + 1;
-
-      totalSelect > 1 ? clearSelected() : "";
-    }
-  }
 
   if (rockBtnIsActive || paperBtnIsActive || scissorBtnIsActive) {
+    allGameBtns.forEach((gameBtn) => {
+      if (gameBtn.classList.contains("selected")) {
+        userResult.textContent = gameBtn.textContent;
+      }
+    });
     return;
   }
-
-  userResult.textContent = "👑";
+  userResult.textContent = gameValues.crowd;
 };
 
 const clearSelected = () => {
   allGameBtns.forEach((gameBtn) => {
     gameBtn.classList.remove("selected");
+    gameBtn.style.pointerEvents = "block";
   });
+
+  togglePlayBtn();
+};
+
+const togglePlayBtn = () => {
+  playBtnGame.classList.toggle("disabled");
 };
 
 const cpuChoose = () => {
-  return Math.floor(Math.random() * (3 - 1 + 1)) + 1;
+  const cpuValue = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
+
+  if (cpuValue === 1) return gameValues.rock;
+
+  if (cpuValue === 2) return gameValues.paper;
+
+  if (cpuValue === 3) return gameValues.scissor;
 };
 
 const userChoose = () => {
@@ -58,6 +88,8 @@ allGameBtns.forEach((gameBtn) => {
 
   gameBtn.addEventListener("mouseout", isSelected);
 
+  gameBtn.addEventListener("click", changeIcon);
+
   gameBtn.addEventListener("click", () => {
     gameBtn.classList.add("selected");
     isSelected();
@@ -67,11 +99,22 @@ allGameBtns.forEach((gameBtn) => {
 playBtnGame.addEventListener("click", startGame);
 
 function startGame() {
-  switch (key) {
-    case value:
-      break;
+  const finalCpuResult = cpuChoose();
+  const finalUserResult = userResult.textContent;
 
+  console.log(finalCpuResult);
+  console.log(finalUserResult);
+
+  switch (finalUserResult) {
+    case gameValues.rock:
+      break;
+    case gameValues.pap:
+      break;
+    case gameValues.scissor:
+      break;
     default:
       break;
   }
 }
+
+function gameLogic(userInput, cpuInput) {}
